78f359d41911e3d830d71536dd07298b22e93db5
jungyunhan <hanjy2008@naver.com>
2022-08-08 22:08:36 +0900

    client test case

--- my-agora-states-client/src/index.test.js
+++ my-agora-states-client/src/index.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from './App';
@@ -6,8 +6,8 @@ import App from './App';
 describe('Bare minimum requirement', () => {
   describe('App.js 컴포넌트 유닛 테스트', () => {
     test('H1 엘리먼트에 "My Agora States" 텍스트 노드가 있어야 합니다.', () => {
-      const { container } = render(<App />);
-      const h1 = container.querySelector('h1');
+      render(<App />);
+      const h1 = screen.getByRole('heading');
       expect(h1.textContent).toBe('My Agora States');
     });
   });
